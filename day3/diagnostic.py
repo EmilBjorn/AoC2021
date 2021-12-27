71fca7faa1a81f9c521bc6b2f3480283ddc4eee0
Emil Bjørn <bjoern.emil@gmail.com>
2021-12-27 23:00:07 +0100

    Refactor of day3 part1

--- day3/diagnostic.py
+++ day3/diagnostic.py
@@ -1,50 +1,71 @@
+
+# %%
+from scipy import stats
+import numpy as np
+
 # %%
 
 
+# def parse(puzzle_input):
+#     """Parse input"""
+#     with open(puzzle_input) as fin:
+#         data = [i for i in fin.read().strip().split("\n")]
+
+#     return data
+
+
+# %%
+
 def parse(puzzle_input):
-    """Parse input"""
     with open(puzzle_input) as fin:
         data = [i for i in fin.read().strip().split("\n")]
 
+        for index, item in enumerate(data):
+            data[index] = [i for i in data[index]]
     return data
 
 
-data = parse("input.txt")
+data = np.array(parse("input.txt"))
 
 # %%
 
 
-def part1(input):
-    posdict = {}
-    for element in input:
-        for i, char in enumerate(element):
-            if i in posdict:
-                posdict[i] += char
-            else:
-                posdict[i] = char
+def part1(data):
+    mode = stats.mode(data).mode[0]
 
-    for item in posdict.items():
-        posdict[item[0]] = item[1].replace('0', '')
-
-    for item in posdict.items():
-        if len(item[1]) > len(input)/2:
-            posdict[item[0]] = '1'
-        else:
-            posdict[item[0]] = '0'
-
-    gamma = ""
-    for item in posdict.items():
-        gamma += item[1]
+    gamma = ''
+    for i in mode:
+        gamma += i
 
     epsilon = ''.join(['1' if i == '0' else '0' for i in gamma])
 
-    gamma = int(gamma, base=2)
-    epsilon = int(epsilon, base=2)
-    print(gamma)
-    print(epsilon)
+    gamma_int = int(gamma, base=2)
+    epsilon_int = int(epsilon, base=2)
 
-    return gamma*epsilon
+    return gamma_int*epsilon_int
+
+
+def part2(data):
+    mode = stats.mode(data).mode[0]
+    print(len(data))
+    for mode_index, mode_item in enumerate(mode):
+        new_data = []
+        for data_index, data_item in enumerate(data):
+            if data_item[mode_index] == mode_item:
+                new_data.append(data_item)
+        data = new_data
+        mode = stats.mode(data).mode[0]
+        print(len(data))
+
+    print(mode, data)
+
+# %%
 
 
 # %%
 part1(data)
+# %%
+part2(data)
+# %%
+
+# %%
