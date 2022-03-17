e4934cbab0292f3cbed51519292d4a40688cdd16
Emil Bjørn <Bjoern.Emil@gmail.com>
2022-03-17 19:53:57 +0100

    BingoPlate parse working prototype

--- day4/bingo.py
+++ day4/bingo.py
@@ -1,21 +1,54 @@
 # %%
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
+
 
 # %%
 
 
 def parse(input):
     with open(input) as f:
-        bingonum = f.readline().split(',')
-        # How to parse Bingo Plates?
+        bingonum = f.readline().strip().split(',')
+
+        bingoPlates = Deck()
+        plateList = []
+        for i, line in enumerate(f):
+            if i >= 2:
+                if not line.strip():
+                    plate = Bingoplate(plateList)
+                    bingoPlates.add(plate)
+                row = line.split()
+                plateList.append(row)
+
+    return bingonum, bingoPlates
+    # How to parse Bingo Plates?
 
 # %%
 # How to store bingo plate?
 
 
 @dataclass
-class Bingoplate():
+class Bingoplate:
 
-    row = []
-    col = []
-    pass
+    rawInput: list
+
+    def __post_init__(self):
+        self.values = {}
+        self.picked = {}
+
+
+@dataclass
+class Deck:
+    plates: List[Bingoplate] = field(init=False)
+
+    def __post_init__(self):
+        self.plates = []
+
+    def add(self, plate):
+        self.plates.append(plate)
+
+
+if __name__ == "__main__":
+    nums, bingoPlates = parse("day4/input.txt")
+    print(nums)
+    print(bingoPlates)
