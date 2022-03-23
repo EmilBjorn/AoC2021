e9644efc6f50c6665bdc62902bb817492f5225c5
Emil Bjørn <Bjoern.Emil@gmail.com>
2022-03-23 16:08:23 +0100

    day4 part1 done

--- day4/bingo.py
+++ day4/bingo.py
@@ -1,64 +1,71 @@
 # -*- coding: iso-8859-1 -*-
 # %%
 from dataclasses import dataclass, field
-from multiprocessing.sharedctypes import Value
-from turtle import st
 from typing import List
-
 
 # %%
 
 
+#TODO (Maybe): Test cases.
 @dataclass
 class Bingoplate:
 
     rawInput: list = field(repr=False)
-
-    @dataclass
-    class Bingonum:
-        value: int
-        picked: bool
+    lastDrawn: int = field(repr=False, default=-1)
 
     def __post_init__(self) -> None:
-        self.number: dict = {}
+        self.new_numbers: dict = {}
         for i, row in enumerate(self.rawInput):
+            # print(row)
             for j, num in enumerate(row):
-                self.number[(i, j, 'val')] = num
-                self.number[(i, j, 'picked')] = False
+                self.new_numbers[(i, j)] = {'val': int(num), 'picked': False}
 
     def __str__(self) -> str:
-        plate = ""
-        for i, row in enumerate(self.rawInput):
-            for j, num in enumerate(row):
-                if int(num) < 10:
-                    strnum = '0'+num
-                else:
-                    strnum = num
+        plate = ''
+        for i in range(5):
+            for key, item in self.new_numbers.items():
+                number = (str(item['val']) if item['val'] > 9 else ' ' +
+                          str(item['val']))
 
-                if self.number[(i, j, 'picked')] == True:
-                    strnum = '('+strnum+')'
-                else:
-                    strnum = ' '+strnum+' '
-                plate += strnum + " "
-                # plate[(i,j)] = Bingonum(num,0)
+                if key[0] == i:
+                    if item['picked'] == True:
+                        plate += '(' + number + ')'
+                    elif item['picked'] == False:
+                        plate += ' ' + number + ' '
+                    else:
+                        test = str(item)
+                        plate += '?' + test + '?'
             plate += '\n'
         return plate.strip('\n')
 
     def bingocheck(self) -> str:
         # Kode som checker om en plade har bingo et sted
         for i in range(5):
-            bingolist = [self.number[i, j, 'picked'] for j in range(5)]
-            # print('Bingolist  række ', i, 'er: ', bingolist)
-            bingolist = [self.number[j, i, 'picked'] for j in range(5)]
-            # print('Bingolist kolonne', i, 'er: ', bingolist)
+            bingolist = [self.new_numbers[(i, j)]['picked'] for j in range(5)]
+            bingolist = [self.new_numbers[(j, i)]['picked'] for j in range(5)]
+
+            #TODO: Er det smart at den både kan returnere bool & int?
             if all(bingolist):
-                return True
+                print('Bingocheck() says BINGO!!')
+                return self.score()
         return False
 
     def pick(self, drawn_num: int) -> None:
-        for key, item in self.number.items():
-            if int(item) == drawn_num:
-                self.number[key[:2]+('picked',)] = True             # key = (?,?,'val')   ||   key[:2]+('picked',) = (?,?,'picked')
+        self.lastDrawn = drawn_num
+        for key, item in self.new_numbers.items():
+            if item['val'] == drawn_num:
+                self.new_numbers[key]['picked'] = True
+
+    def score(self):
+        score = 0
+        #TODO: Gør disse ranges dynamiske ifht størrelsen på bingo-pladen
+        for i in range(5):
+            for j in range(5):
+                if not self.new_numbers[(i, j)]['picked']:
+                    score += self.new_numbers[(i, j)]['val']
+
+        score *= self.lastDrawn
+        return score
 
 
 @dataclass
@@ -71,18 +78,30 @@ class Deck:
     def add(self, plate):
         self.plates.append(plate)
 
+    def draw(self, drawn_number: int):
+        # Kode som looper over alle trukkede numre og checker om der er bingo
+        for plate in self.plates:
+            plate.pick(drawn_number)
+            bingo = plate.bingocheck()
+            if bool(bingo):
+                print(plate)
+                break
+        return bingo
+
+
 # %%
 
 
 def parse(input):
     with open(input) as f:
         drawnNums = f.readline().strip().split(',')
+        drawnNums = [int(i) for i in drawnNums]
 
         bingoPlates = Deck()
         plateList = []
         for i, line in enumerate(f):
             if i > 0:
-                if line.strip() == "":
+                if line.strip() == '':
                     plate = Bingoplate(plateList)
                     bingoPlates.add(plate)
                     plateList = []
@@ -93,35 +112,20 @@ def parse(input):
     return drawnNums, bingoPlates
 
 
-def play_bingo(drawn_numbers: list):
-    # Kode som gennemgår alle trukkede numre og checker om der er bingo
-    #   Skal måske være en metode i Deck klassen.
-    pass
+def part1():
+    nums, bingoPlates = parse('day4/input.txt')
+
+    bingoPlates.plates = bingoPlates.plates
+    print(bingoPlates.plates)
+
+    for i in nums:
+        print('Drawing: ', i)
+        bingo = bingoPlates.draw(i)
+        if bingo:
+            break
+    print(bingo)
 
 
 # %%
-if __name__ == "__main__":
-    nums, bingoPlates = parse("day4/input.txt")
-    # print(nums)
-    # for i in range(3):
-    print(bingoPlates.plates[0])
-
-    bingoPlates.plates[0].number[0, 1, 'picked'] = True
-    bingoPlates.plates[0].number[0, 2, 'picked'] = True
-    bingoPlates.plates[0].number[0, 4, 'picked'] = True
-    bingoPlates.plates[0].number[4, 0, 'picked'] = True
-    bingoPlates.plates[0].number[1, 2, 'picked'] = True
-    bingoPlates.plates[0].number[2, 2, 'picked'] = True
-    bingoPlates.plates[0].number[3, 2, 'picked'] = True
-    print()
-    bingoPlates.plates[0].bingocheck()
-    bingoPlates.plates[0].pick(96)
-    print(bingoPlates.plates[0])
-    bingoPlates.plates[0].bingocheck()
-
-    # print("\n")
-    # for j in range(5):
-    #     bingolist = [bingoPlates.plates[0].number[i, j, 'picked'] for i in range(5)]
-    #     print('Bingolist er: ', bingolist)
-    #     if all(bingolist):
-    #         print('BINGO!!')
+if __name__ == '__main__':
+    part1()
